a574aee021660fd5434bbe1d215dea5a463e3fac
Alan Jeffrey <ajeffrey@mozilla.com>
2018-08-13 10:26:34 -0500

    Trying to get arm32 build to work

---
+++ support/arm32/fake-ld.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+arm-linux-gnueabihf-gcc -Lsupport/arm32/usr/lib/arm-linux-gnueabihf $*
